d4208235c19a1489918a2a6dc825f7e93c372088
thedonut3054 <136283010+thedonut3054@users.noreply.github.com>
2025-12-03 17:13:59 +0000

    Simple class instantiation of the dataframe as an object

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import pandas as pd
-df = pd.read_csv("/workspaces/Coding-2-PANDAS-Project/vgsales.csv")
-df.head()
+class data:
+    def __init__(self, csv):
+        self.df = pd.read_csv(csv)
+df = data("/workspaces/Coding-2-PANDAS-Project/vgsales.csv")
+print(df.df.head(50))
