1b7d4464b55ccf5a7a1165c3030caeeded7362ef
thedonut3054 <136283010+thedonut3054@users.noreply.github.com>
2025-12-05 18:05:50 +0000

    more filtering options

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 class data:
     def __init__(self, csv):
         self.df = (pd.read_csv(csv))
+        self.GSFO_Greater_Than = True
+        self.GSFO_Sales_Amount = 0
     def getInt(self):
         self.df[["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"]] = self.df[["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"]].apply(lambda y: y*1000000)
         self.df[["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"]] = self.df[["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"]].astype(int)
@@ -26,6 +28,7 @@ class data:
                     self.sortMain()
                 # elif o == 2:
                 #     self.sortMain() no need for two of these
+                    break
             except:
                 pass
     def filterMain(self):
@@ -47,11 +50,11 @@ class data:
                 pass
     def sortMain(self):
         print("DataFrame Sorting Options") # How are we going to sort by multiple things wont the sorts override each other?
+        print("DataFrame Sorting Options: ")
         print("  1. Sales")
-        print("  2. Genre")
-        print("  3. Year")
-        print("  4. Platform")
-        print("  5. Save and return")
+        print("  2. Year")
+        print("  3. Title")
+        print("  4. Save and return")
         print()
         o = None
         while True:
@@ -89,23 +92,28 @@ class data:
         print("Make Sales Integers")
         print("Make Sales Floats")
         print("other option idk")
-    # def filterSales(self): 
-    #     pass
-    # def sortMain():
-    #     print("DataFrame Sorting Options")
-    #     print("  1. Sales")
-    #     print("  2. Genre")
-    #     print("  3. Year")
-    #     print("  4. Platform")
-    #     print("  5. Save and return")
-    #     print()
-    #     o = None
-    #     while True:
-    #         try:
-    #             o = int(input("Enter the number of the option you want to select: "))
-    #         except:
-    #             pass
-# im pretty sure lines 66 through 81 should not be here? so ive comented them out for now
-
+    def filterSales(self): 
+        print("DataFrame Sales Filter Options: ")
+        print("  1. Global Sales")
+        print("  2. NA Sales")
+        print("  3. EU Sales")
+        print("  4. JP Sales")
+        print("  5. Other Sales")
+        print("  6. Save and return")
+        print()
+        o = None
+        while True:
+            try:
+                o = int(input("Enter the number of the option you want to select: "))
+                if o == 1:
+                    self.filterGlobalSales()
+            except:
+                pass        
+    def filterGlobalSales(self):
+        def GSFOptions():
+            if self.GSFO_Greater_Than == True:
+                return(f"GREATER THAN {self.GSFO_Sales_Amount}")
+        print("DataFrame Global Sales Filter Options: ")
+        print(f"  Only Show entries with {GSFOptions()} sales")
 df = data("/workspaces/Coding-2-PANDAS-Project/vgsales.csv")
 df.options() # what is this here for
